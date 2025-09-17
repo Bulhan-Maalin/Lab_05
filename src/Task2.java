f953f427dc96905c627c147b0325c3b0569a1a51
622140 <maalin.622140@swl.k12.oh.us>
2025-09-17 08:55:53 -0400

    Finished Task 2

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int birthMonth = 0;
+
+        System.out.println("Please enter the number of your birth month:");
+        if (scan.hasNextInt()) {
+            birthMonth = scan.nextInt();
+        }
+        else {
+            System.out.println("Error - Invalid Input");
+            System.exit(0);
+        }
+
+        if (birthMonth >= 1 && birthMonth <= 12) {
+            System.out.println("Your birth month is: " + birthMonth + ".");
+        }
+        else {
+            System.out.println("Error - Invalid Input");
+            System.exit(0);
+        }
+    }
 }
