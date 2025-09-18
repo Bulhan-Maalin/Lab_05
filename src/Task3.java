f8eb3421c7f4b0f42a6f82cc5355eec3805673cd
622140 <maalin.622140@swl.k12.oh.us>
2025-09-18 08:24:07 -0400

    Finished Task 3

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Task3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String userInput = "";
+
+        System.out.println("Please enter your party affiliation. Enter a R for republican, D for Democrat, or I for independent:");
+        userInput = scan.nextLine();
+
+        if (userInput.equals("D")) {
+            System.out.println("You get a Democratic Donkey.");
+        } else if (userInput.equals("R")) {
+            System.out.println("You get a Republican Elephant.");
+        } else if (userInput.equals("I")) {
+            System.out.println("You get an Independent Person.");
+        } else {
+            System.out.println("Error - Invalid Input");
+            System.exit(0);
+        }
+
+    }
 }
+
+
+
