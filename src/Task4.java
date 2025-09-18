182f09f33def6d1a17b433a1ad2f0a82cf550ff7
622140 <maalin.622140@swl.k12.oh.us>
2025-09-18 08:29:38 -0400

    Finished Task 4

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Task4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int age = 0;
+
+        System.out.println("Please enter your age:");
+        if (scan.hasNextInt()) {
+            age = scan.nextInt();
+        }
+        else {
+            System.out.println("Error - Invalid Input");
+            System.exit(0);
+        }
+
+        if (age >= 21) {
+            System.out.println("Here is your wristband. Enjoy the movie!");
+        }
+        else if (age < 21) {
+            System.out.println("Enjoy the movie!");
+        }
+        else {
+            System.out.println("Error - Invalid Input");
+            System.exit(0);
+        }
+
+    }
 }
